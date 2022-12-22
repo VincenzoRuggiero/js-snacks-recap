59047e8df3104eb2c17c6e9d22aacc298463d5c2
VincenzoRuggiero <vincenzoru@live.it>
2022-12-22 10:20:50 +0100

    Fix

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
 const { createApp } = vue;
 
 createApp({
-    date(){
-        return {
-            evenNum: [],
-            oddsNum: [],
-        }
-    }
-   methods: {
-    getNum(){
-        axios
+  date() {
+    return {
+      evenNum: [],
+      oddsNum: [],
+    };
+  },
+  methods: {
+    getNum() {
+      axios
         .get("https://flynn.boolean.careers/exercises/api/random/int")
         .then((result) => {
-          if(result % 2 === 0){
-            this.evenNum.push(result)
+          const number = result.data.response;
+          if (number % 2 === 0) {
+            this.evenNum.push(number);
           } else {
-            this.oddsNum.push(result)
+            this.oddsNum.push(number);
           }
         });
-    }
-   },
-}).mount("#app")
+    },
+  },
+}).mount("#app");
